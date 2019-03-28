9ff132aa8ef33e81c7e89cc852e224a3e7ef72b5
Denis Grigor <apprentice3d@gmail.com>
2019-03-28 15:08:45 +1100

    basic attribute adder

--- www/js/xlsextension.js
+++ www/js/xlsextension.js
@@ -1,5 +1,6 @@
 function XLSExtension(viewer, options) {
   Autodesk.Viewing.Extension.call(this, viewer, options);
+  this.data = {"Columns":[]};
 }
 
 XLSExtension.prototype = Object.create(Autodesk.Viewing.Extension.prototype);
@@ -62,6 +63,73 @@ XLSExtension.prototype.load = function () {
 
 
     _viewer.toolbar.addControl(this.subToolbar);
+
+
+
+
+    // Denis code
+
+    this.ui = document.createElement("div");
+    this.ui.id = "control_area";
+    this.ui.classList.add("docking-panel-container-solid-color-a");
+    this.ui.innerHTML = `
+            <div id="controlsArea">
+                <div><span>BGEReinforcement: </span><input type="text" value=0 id="BGEReinforcement"></div>
+                <div><span>BGELoadTensionMax: </span><input type="text" value=0 id="BGELoadTensionMax"></div>
+                <div><span>BGELoadLiveMax: </span><input type="text" value=0 id="BGELoadLiveMax"></div>
+                <div><span>BGELoadDeadMax: </span><input type="text" value=0 id="BGELoadDeadMax"></div>
+                <button id="commit">Commit</button>
+                <button id="send">send</button>
+            </div>
+        `;
+
+    let panel = this.panel;
+    // check https://forge.autodesk.com/blog/extension-skeleton-toolbar-docking-panel
+    let toolbarButtonPropertyAdder = new Autodesk.Viewing.UI.Button('PropertyAdder');
+
+    if (panel == null) {
+      panel = new DataAddingPanel(_viewer, _viewer.container,
+          'controlPanel', 'Control Panel', {"innerDiv":this.ui});
+    }
+
+
+
+    // bindings
+    let controller1 = document.getElementById("BGEReinforcement");
+    let controller2 = document.getElementById("BGELoadTensionMax");
+    let controller3 = document.getElementById("BGELoadLiveMax");
+    let controller4 = document.getElementById("BGELoadDeadMax");
+
+
+    let CommitButton = document.getElementById("commit");
+
+    CommitButton.onclick = () => {
+      this.data["Columns"].push({
+        "ColumnId": this.viewer.getSelection()[0],
+        'BGEReinforcement': controller1.value,
+        'BGELoadTensionMax': controller2.value,
+        'BGELoadLiveMax': controller3.value,
+        'BGELoadDeadMax': controller4.value
+      });
+
+
+    };
+
+
+    let SendButton = document.getElementById("send");
+
+    SendButton.onclick = () => {
+      fetch("/da4revit/v1/columns",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.data),
+          });
+    }
+
+
   };
 
   createUI();
@@ -98,3 +166,29 @@ function upgradeColumnInfo(fileItemId, fileItemName) {
 }
 
 Autodesk.Viewing.theExtensionManager.registerExtension('Autodesk.Sample.XLSExtension', XLSExtension);
+
+
+
+// *******************************************
+// Data Adding Panel
+// *******************************************
+function DataAddingPanel(viewer, container, id, title, options) {
+  this.viewer = viewer;
+
+  Autodesk.Viewing.UI.DockingPanel.call(this, container, id, title, options);
+
+  // the style of the docking panel
+  // use this built-in style to support Themes on Viewer 4+
+
+  this.container.appendChild(options.innerDiv);
+  this.container.style = `
+    bottom: 10px;
+    right: 10px;
+    width: 300px;
+    height: 200px;
+    resize: none;
+    `
+
+}
+DataAddingPanel.prototype = Object.create(Autodesk.Viewing.UI.DockingPanel.prototype);
+DataAddingPanel.prototype.constructor = DataAddingPanel;
